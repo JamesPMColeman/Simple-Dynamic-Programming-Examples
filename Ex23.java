78ca1d430164c20875609a54b2c678abff52e204
James Coleman <jcolem22@msudenver.edu>
2020-10-22 00:09:26 -0600

    The matrix is now filling out properly. All I have left is two display the strings with their new '-'s.

--- Ex23.java
+++ Ex23.java
@@ -3,14 +3,43 @@ import java.util.Scanner;
 public class Ex23 {
 
 	private static int[][] matrix;
-	private static char[][] path;
+	private static String[][] path;
 	private static String a, b;
 
 	private static int score(int x, int y) {
-		return 1;
+		int diagonal = matrix[x - 1][y - 1] + match(a.charAt(x), b.charAt(y)); 
+		int up = 2 + matrix[x - 1][y];
+		int left = 2 + matrix[x][y - 1];
+		// I get the feeling this one is unnecessary 
+		if (diagonal == up && up == left) {
+			path[x][y] = "D/U/L";
+			return diagonal;
+		}
+		if (diagonal == up && up < left) {
+			path[x][y] = "D/U";
+			return diagonal;
+		}
+		if (diagonal == left && up > left) {
+			path[x][y] = "D/L";
+			return diagonal;
+		}
+		if (diagonal <= up && diagonal <= left) {
+			path[x][y] = "D";
+			return diagonal;
+		}
+		if (up < diagonal && up < left) {
+			path[x][y] = "U";
+			return up;
+		}
+		if (left < up && left < diagonal) {
+			path[x][y] = "L";
+			return left;
+		}
+		return -1;
 	}
 
 	private static int match(char a, char b) {
+		if (a == '-' && b != '-' || a != '-' && b == '-') return 2;
 		if (a != b) return 1;
 		return 0;
 	}
@@ -19,19 +48,21 @@ public class Ex23 {
 		
 		if (x == 0) {
 			matrix[x][y] = 2 * y + match(a.charAt(x), b.charAt(y)); 
-			path[x][y] = 'B';
+			path[x][y] = "B";
 		}
 		else if (y == 0) {
 			matrix[x][y] = 2 * x + match(a.charAt(x), b.charAt(y)); 
-			path[x][y] = 'B';
+			path[x][y] = "B";
 		}
 		else {
-			System.out.println(x + ", " + y);
 			matrix[x][y] = score(x, y);
 		}
 		if (x == a.length() - 1 && y == b.length() - 1) return;
 		else if (y == b.length() - 1) computeSequence(++x, 0);
 		else computeSequence(x, ++y);	
+	}
+	private static void traceBack() {
+		System.out.println("This is the end");
 	}
 
 	private static boolean valid(String a, String b) {
@@ -54,23 +85,23 @@ public class Ex23 {
 			System.out.println("Something seems off with your DNA sequences. Please try again");
 			processInput();
 		}
-		matrix = new int[temp1.length()][temp2.length()];
-		path = new char[temp1.length()][temp2.length()];
-		a = temp1;
-		b = temp2;
+		a = temp1 + "-";
+		b = temp2 + "-";
+		matrix = new int[a.length()][b.length()];
+		path = new String[a.length()][b.length()];
 		System.out.println("a length: " + a.length());
 		System.out.println("b length: " + b.length());
 	}
 
 	private static void printMatrix() {
 		System.out.print("    ");
-        for (int col=0; col < b.length(); col++) System.out.printf("%12c", b.charAt(col) );
+        for (int col=0; col < b.length(); col++) System.out.printf("%13c", b.charAt(col));
 
         System.out.println("\n");
         for (int row=1; row<=a.length(); row++) {
             System.out.printf("%4c", a.charAt(row - 1));
             for (int col=1; col<=b.length(); col++) {
-                System.out.printf("%8d(%2c)", matrix[row-1][col-1], path[row-1][col-1] );
+                System.out.printf("%8d(%3s)", matrix[row-1][col-1], path[row-1][col-1] );
             }
             System.out.println("\n");
         }
@@ -80,5 +111,6 @@ public class Ex23 {
 		processInput();
 		computeSequence(0, 0);
 		printMatrix();
+		traceBack();
 	}
 }
